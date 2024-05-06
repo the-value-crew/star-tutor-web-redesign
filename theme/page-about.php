8fcab5e71702d03e6a647fc2c2f96fd3e17b21cf
bigyanse <contact.bigyanse@gmail.com>
2024-05-06 17:29:37 +0545

    `fix`: 🐛 🎨 fixed border radius around images in the slider

--- theme/page-about.php
+++ theme/page-about.php
@@ -6,7 +6,7 @@
 
 get_header();
 
-$about_bg_img = get_field( 'hero_section_image' ); // thestartutor_get_static_img( 'about-bg.png' );
+$about_bg_img = thestartutor_get_static_img( 'about-bg.png' );
 
 $aboutme_args     = array(
 	'post_type' => 'about_me_content',
@@ -35,20 +35,6 @@ $myhobby_wp_query = new WP_Query( $myhobby_args );
 				</div>
 			</div>
 	</section>
-	<!-- <section class="relative h-[350px] overflow-hidden">
-		<div
-			class="absolute inset-0"
-			style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.30) 0%, rgba(0, 0, 0, 0.00) 66.62%), linear-gradient(0deg, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 0, 0.50) 100%), url('<?php echo $about_bg_img; ?>'), lightgray 50% / cover no-repeat; background-position: center; background-size: cover; background-repeat: no-repeat; animation: imageZoomIn 0.5s ease-in-out forwards;">
-			<div
-				class="2xl:container p-[16px] md:px-[80px] lg:px-[40px] xl:px-[200px] 2xl:px-[418px] flex flex-col justify-center text-title font-title">
-				<div class="text-primary-dark">
-					<p class="text-title font-title">Learn <span class="text-brand-invert">about</span></p>
-					<p class="text-title font-title">The Star Tutor</p>
-				</div>
-			</div>
-		</div>
-	</section> -->
-
 
 	<section class="2xl:container  flex flex-col items-start py-[120px] p-[16px] md:px-[80px] lg:px-[40px] xl:px-[200px] 2xl:px-[418px]">
 			<div class="w-full relative">
